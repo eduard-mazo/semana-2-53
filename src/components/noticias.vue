ae7e407b0cab6399795fe58669b64888318f56df
Eduard Mazo <eduard.mazo@gmail.com>
2020-12-06 11:16:14 -0500

    Using vue hooks on components

--- src/components/noticias.vue
+++ src/components/noticias.vue
@@ -1,74 +1,58 @@
 <template>
     <div class= "container mt-3">
-        <!-- INICIO SECCION 3 "NEWS" -->
         <section id="news">
             <header class="section-title text-center container p-3 my-3 bg-dark text-white">
                 <h2>NOTICIAS</h2>
             </header>
             <div class="container">
-
-                <div class="main row mt-3  border">
-                    <div id= "poster1" class = "col-sm-2"></div>
-
-                    <div class = "col-sm-4">
-                        <div id= "title1" class = "h3 text-uppercase"></div>
-                        <div id= "year1" class = "h4"></div>
-                        <div id= "actors1" class = "h4"></div>
-                        <div class = "mt-3">
-                            <a href="https://www.imdb.com" target="_blank" class="btn btn-secondary">Información</a>
-                        
+                <div class="row mt-3">
+                    <div v-for="movie in movies" :key="movie.imdbID" class="col-sm-6">
+                        <div class="card flex-row">
+                            <div class="card-header col-sm-4">
+                                <img v-bind:src="movie.Poster" alt="" width="150" height="225" >
+                            </div>
+                            <div class="card-body col-sm-8">
+                                <h4 class="card-title">{{movie.Title}}</h4>
+                                <p class="card-text">{{movie.Plot}}</p>
+                            </div>
                         </div>
                     </div>
-
-                    <div id= "poster2" class = "col-sm-2 "></div>
-
-                    <div class = "col-sm-4">
-                        <div id= "title2" class = "h3 text-uppercase"></div>
-                        <div id= "year2" class = "h4"></div>
-                        <div id= "actors2" class = "h4"></div>
-                        <div class = "mt-3" >
-                            <a href="https://www.imdb.com" target="_blank" class="btn btn-secondary">Información</a>
-                        </div>
-                    </div>
-                    
                 </div>
-
-                <div class="main row mt-3  border">
-                    <div id= "poster3" class = "col-sm-2"></div>
-
-                    <div class = "col-sm-4 text-justify">
-                        <div id= "title3" class = "h3 text-uppercase"></div>
-                        <div id= "year3" class = "h4"></div>
-                        <div id= "actors3" class = "h4"></div>
-                        <div class = "mt-3">
-                        <a href="https://www.imdb.com" target="_blank" class="btn btn-secondary">Información</a>
-                        </div>
-                    </div>
-
-                    <div id= "poster4" class = "col-sm-2"></div>
-
-                    <div class = "col-sm-4 text-justify">
-                        <div id= "title4" class = "h3 text-uppercase"></div>
-                        <div id= "year4" class = "h4"></div>
-                        <div id= "actors4" class = "h4"></div>
-                       <div class =  "mt-3">
-                        <a href="https://www.imdb.com" target="_blank" class="btn btn-secondary">Información</a>
-                    </div>
-                    </div>
-                    
-                </div>
-                
             </div>
 
         </section>
-        <!-- INICIO SECCION 3 "NEWS" -->
     </div>
 </template>
 
 <script>
     export default {
-        name: "TeamCard",
-        props: ['member']
+        name: "Noticias",
+        beforeMount() {
+            this.getFilm();
+        },
+        data: () => ({
+            movies: {},
+            titles: ["up", "fast", "harry","her"]
+        }),
+        methods: {
+            reload() {
+                this.$forceUpdate();
+                },
+            getFilm() {
+                this.titles.forEach(movie => {
+                    let urlOmbd = `http://www.omdbapi.com/?apikey=964334f2&t="${movie}"&type=movie`;
+                    let vueData = this.movies;
+                    fetch(urlOmbd, {method:"GET"})
+                    .then(res => res.json())
+                    .then(data => {
+                        vueData[movie] = data;
+                        console.log(vueData);
+                        this.reload();
+                    });
+                });
+                
+            }
+        }
     }
 </script>
 
